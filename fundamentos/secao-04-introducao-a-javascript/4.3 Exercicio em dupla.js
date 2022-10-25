9a21ccca0bd92d5ed5fa40f9385ef2c4ef3053b3
rodolfohro <rodolfohro@gmail.com>
2022-10-25 13:19:38 -0300

    Aula 4.3

---
+++ fundamentos/secao-04-introducao-a-javascript/4.3 Exercicio em dupla.js
@@ -0,0 +1,82 @@
+// Simulação de um sistema de login feito em javascript
+
+let userName = 'Tamara Simões';
+let userPassword = 1234;
+let acess;
+
+// |Validar se o usuário e senha passados são válidos
+
+if (userName === 'Tamara Simões' && userPassword === 1234){
+  acess = true; 
+  console.log('Acesso permitido')
+}
+else {
+  console.log('Acesso negado');
+}
+
+console.log(acess);
+
+
+// Caso acesso permitido, crie um recurso q leia  a quantidades de pães disponiveis e adicioneos em uma sacola. Faça com que o recurso mostre quantos pães foram adicionados 1 por 1
+// se não permitido o acesso mostre uma msg de erro
+
+let paes = 10;
+let sacola = 0;
+
+if (acess === true) {
+  for (let i = 1; i <= paes; i++) {
+    sacola ++ ;
+    console.log('Item Adicionado: ', sacola);
+  }
+}
+else {
+  console.log('você não pode comprar mais pães');
+
+}
+
+
+ // 1 - Faça um algoritmo que calcule a soma de 1 a 50 usando a estrutura "for" e retorne no formato:
+// A soma total de 1 a 50 é: X
+ 
+let soma = 0;
+for (let i = 1; i <= 50; i++) {
+ soma += i;
+}
+console.log(soma);
+
+
+// 2 - Crie um algoritmo que conte quantos números do intervalo entre 2 e 150 são divisíveis por 3.
+// Caso a quantidade seja igual a 50, exiba uma mensagem secreta.
+
+
+let quantNumeros = 0;
+
+
+for (let i = 2; i <= 150; i++) 
+if (i%3 === 0) {
+  quantNumeros ++;
+}
+
+console.log(quantNumeros);
+if (quantNumeros === 50) {
+  console.log('mensagem secreta');
+}
+
+// 5 - Crie um algoritmo que recebe a idade de Carolzita, Murilo e Baêta e imprime quem é a pessoa mais nova no formato:
+// "Pessoa" é a mais nova.
+
+var pessoas = {
+ Carolzita: 23,
+ Baêta: 60,
+ Murilo: 18,
+
+}
+let maisNovo = Infinity;
+
+for (var i in pessoas) {
+ if (pessoas[i] < maisNovo) {
+   maisNovo = pessoas[i];
+ }
+}
+
+console.log(i);
