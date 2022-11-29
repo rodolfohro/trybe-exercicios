f00cd64237612cbdccc16e09dcc7913bc6bb282b
rodolfohro <rodolfohro@gmail.com>
2022-11-29 16:05:32 -0300

    Dia 1 Reumo forEach

---
+++ fundamentos/secao-08-Higher-Order-Functions-do-JavaScript-ES6/Dia 01: Introdução a Higher Order Functions/Exercícios/listDeEmail.js
@@ -0,0 +1,16 @@
+//Use o método forEach para exibir a lista de emails com a seguinte frase: O email ${email} está cadastrado em nosso banco de dados!.
+
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+// Adicione seu código aqui
+
+const enviarEmail = (email) =>{
+	console.log (`O email ${email} está cadastrado em nosso banco de dados!.`)
+}
+
+emailListInData.forEach(enviarEmail);
